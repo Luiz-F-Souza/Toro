cfae1bd1c8688667a16c41edb5c6cdd099ff5edb
Luiz Felipe Souza <luizfelipesouza1998@outlook.com>
2023-08-31 08:23:58 -0300

    Criação do App.tsx e main.tsx

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { HomePage } from "./Pages/Home/HomePage"
+import { Header } from "@components/Header/Header"
+
+
+function App() {
+
+
+  return (
+    <div>
+      <Header />
+      <HomePage />
+    </div>
+  )
+}
+
+export default App
