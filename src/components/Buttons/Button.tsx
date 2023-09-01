ed1b855842b8f63191befaebd24e8e060790191b
Luiz Felipe Souza <luizfelipesouza1998@outlook.com>
2023-09-01 06:25:14 -0300

    formatação e adição do tailwindMerge para estilização por sobreposição de classes

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -1,24 +1,26 @@
-interface Props extends React.HTMLAttributes<HTMLButtonElement> {
+import { twMerge } from "tailwind-merge";
+import { HTMLAttributes } from "react";
+
+interface Props extends HTMLAttributes<HTMLButtonElement> {
   children: string;
-  variant?: 'solid' | 'outlined'
+  variant?: "solid" | "outlined";
 }
 
 export const Button: React.FC<Props> = ({
   children,
-  variant = 'solid',
+  variant = "solid",
   className = "",
   ...props
 }) => {
   return (
     <button
-      {...props}
-      className={`
-        ${className}  
-        ${ 
-          variant === "solid" ? 
-            "text-neutral-lightest bg-brand-primary" :
-            "text-brand-primary border-brand-primary border-1 hover:text-neutral-lightest" 
-          }
+      className={twMerge(
+        `  
+        ${
+          variant === "solid"
+            ? "text-neutral-lightest bg-brand-primary"
+            : "text-brand-primary border-brand-primary border-1 hover:text-neutral-lightest"
+        }
         w-[9.375rem] 
         text-sm font-semibold
         rounded 
@@ -26,7 +28,10 @@ export const Button: React.FC<Props> = ({
         hover:bg-brand-primary-dark 
         active:bg-brand-primary
         transition-colors duration-200
-      `}
+      `,
+        className
+      )}
+      {...props}
     >
       {children}
     </button>
