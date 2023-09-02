b3d3d1f6e59a26221a57916235f052442395579b
Luiz Felipe Souza <luizfelipesouza1998@outlook.com>
2023-09-02 18:10:59 -0300

    feat: adiciona link para os itens de menu

--- src/components/Header/HeaderDesktopNavigation.tsx
+++ src/components/Header/HeaderDesktopNavigation.tsx
@@ -1,5 +1,5 @@
-import userIcon from "../../assets/icons/user-account.svg";
-import { Button } from "../Buttons/Button";
+import userIcon from "@assets/icons/user-account.svg";
+import { LinkButton } from "../Buttons/LinkButton";
 import { Link } from "react-router-dom";
 
 export const HeaderDesktopNavigation = () => {
@@ -8,37 +8,64 @@ export const HeaderDesktopNavigation = () => {
       <nav className="hidden lg:block">
         <ul className="flex gap-8 xl:gap-10">
           <li>
-            <Link to="/bolsa" className="text-gray-07">
+            <Link to="/bolsa" className="text-gray-07 hover:text-brand-primary">
               Bolsa
             </Link>
           </li>
           <li>
-            <p className="text-gray-07">Renda Fixa</p>
+            <Link
+              to="/renda-fixa"
+              className="text-gray-07 hover:text-brand-primary"
+            >
+              Renda Fixa
+            </Link>
           </li>
           <li>
-            <p className="text-gray-07">Fundos</p>
+            <Link
+              to="/fundos"
+              className="text-gray-07 hover:text-brand-primary"
+            >
+              Fundos
+            </Link>
           </li>
           <li>
-            <p className="text-gray-07">Plataformas de Trading </p>
+            <Link
+              to="/plataformas"
+              className="text-gray-07 hover:text-brand-primary"
+            >
+              Plataformas de Trading{" "}
+            </Link>
           </li>
           <li>
-            <p className="text-gray-07">Aprenda</p>
+            <Link
+              to="/aprenda"
+              className="
+                text-gray-07 hover:text-brand-primary
+              "
+            >
+              Aprenda
+            </Link>
           </li>
           <li>
-            <p className="text-gray-07">Simulador</p>
+            <Link
+              to="/simulador"
+              className="text-gray-07 hover:text-brand-primary"
+            >
+              Simulador
+            </Link>
           </li>
         </ul>
       </nav>
 
       <section className="hidden lg:flex gap-4 items-center">
-        <button className="flex gap-3 items-center">
-          <img src={userIcon} alt="" />
+        <Link to="/entrar" className="flex gap-3 items-center">
+          <img src={userIcon} width={25} height={25} alt="" />
           <p className="text-sm text-gray-08 font-normal">Fazer login</p>
-        </button>
+        </Link>
 
-        <Button variant="outlined" className="h-auto">
+        <LinkButton to="/entrar" variant="outlined" className="h-auto">
           Abra sua conta
-        </Button>
+        </LinkButton>
       </section>
     </>
   );
