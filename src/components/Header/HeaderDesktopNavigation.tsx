21adaca362a6391fdb1c71fe49c986f5d5a738fe
Luiz Felipe Souza <luizfelipesouza1998@outlook.com>
2023-09-01 23:07:53 -0300

    altura do botão setada para auto

--- src/components/Header/HeaderDesktopNavigation.tsx
+++ src/components/Header/HeaderDesktopNavigation.tsx
@@ -1,5 +1,6 @@
 import userIcon from "../../assets/icons/user-account.svg";
 import { Button } from "../Buttons/Button";
+import { Link } from "react-router-dom";
 
 export const HeaderDesktopNavigation = () => {
   return (
@@ -7,7 +8,9 @@ export const HeaderDesktopNavigation = () => {
       <nav className="hidden lg:block">
         <ul className="flex gap-8 xl:gap-10">
           <li>
-            <p className="text-gray-07">Bolsa</p>
+            <Link to="/bolsa" className="text-gray-07">
+              Bolsa
+            </Link>
           </li>
           <li>
             <p className="text-gray-07">Renda Fixa</p>
@@ -33,22 +36,9 @@ export const HeaderDesktopNavigation = () => {
           <p className="text-sm text-gray-08 font-normal">Fazer login</p>
         </button>
 
-        {/* <button
-          className="
-          w-[9.375rem] 
-          border-brand-primary border-1 
-          text-brand-primary 
-          rounded 
-          px-4 py-3
-          hover:bg-brand-primary-dark hover:text-neutral-lightest
-          active:bg-brand-primary
-          transition-colors duration-200
-          "
-        >
-          <p className="">Abra sua conta</p>
-        </button> */}
-
-        <Button variant="outlined">Abra sua conta</Button>
+        <Button variant="outlined" className="h-auto">
+          Abra sua conta
+        </Button>
       </section>
     </>
   );
